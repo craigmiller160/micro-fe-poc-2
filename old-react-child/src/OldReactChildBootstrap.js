1b75b81150f633d48cda4a5e28410aa50e269d7f
Craig <craigmiller160@gmail.com>
2021-08-14 15:02:52 -0400

    More progress with old react child

---
+++ old-react-child/src/OldReactChildBootstrap.js
@@ -0,0 +1,4 @@
+import OldReactChildApp from './components/OldReactChildApp';
+import { wrapAndRegisterWebComp } from './createWebComp';
+
+wrapAndRegisterWebComp('old-react-child', OldReactChildApp);
