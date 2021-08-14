e362e188b95dd6ed2f3fe87c7c153f4b2d580791
Craig <craigmiller160@gmail.com>
2021-08-14 15:15:04 -0400

    Finished the new react child

---
+++ new-react-child/src/NewReactChildBootstrap.js
@@ -0,0 +1,4 @@
+import NewReactChildApp from './components/NewReactChildApp';
+import { wrapAndRegisterWebComp } from './createWebComp';
+
+wrapAndRegisterWebComp('new-react-child', NewReactChildApp);
